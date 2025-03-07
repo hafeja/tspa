4928857f8c659b61e4b1839bddc1be7b8770e2cf
Foo Bar <FOOBAR>
2025-03-07 11:33:12 +0100

    Remove asserPrefix from next config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   output: "export",  // <=== enables static exports for GitHub Pages
   reactStrictMode: true,
   basePath: "/tspa",
-  assetPrefix: "/tspa",
 };
 
 export default nextConfig;
