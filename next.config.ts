8b4b4857125d056965b9222a7927cccd9afdeb0d
Foo Bar <FOOBAR>
2025-03-07 09:06:21 +0100

    Add GitHub action for build & deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",  // <=== enables static exports for GitHub Pages
+  reactStrictMode: true,
+  basePath: "/tspa",
 };
 
 export default nextConfig;
