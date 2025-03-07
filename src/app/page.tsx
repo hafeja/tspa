4ed555bae51aad30790ed86278c78c5690ce577f
Foo Bar <FOOBAR>
2025-03-07 11:21:07 +0100

    Fix syntax issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import nextConfig from "../../next.config"
-console.log(basePath);
 
 export default function Home() {
   return (
