f884e732e2ba8af1c45e0963f009a129180ecbef
Foo Bar <FOOBAR>
2025-03-07 10:35:52 +0100

    Disable image optimization

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",  // <=== enables static exports for GitHub Pages
   reactStrictMode: true,
+  basePath: "/tspa",
+};
+
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
