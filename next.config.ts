b3b20797563d3c8dcf8f83a1c12872bb5006073c
Foo Bar <FOOBAR>
2025-03-07 10:16:03 +0100

    remove baseUrl from next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",  // <=== enables static exports for GitHub Pages
   reactStrictMode: true,
-  basePath: "/tspa",
 };
 
 export default nextConfig;
